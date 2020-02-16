4df875edc7dd561abe8a2e07826f5928ec4bd454
Aryan Sharma <aryan@smartsense.co.in>
2020-02-16 10:02:29 +0530

    Initial Commit

---
+++ BFSlidingButton/BFSlidingButton.h
@@ -0,0 +1,19 @@
+//
+//  BFSlidingButton.h
+//  BFSlidingButton
+//
+//  Created by Aryan Sharma on 16/02/20.
+//  Copyright © 2020 Aryan Sharma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BFSlidingButton.
+FOUNDATION_EXPORT double BFSlidingButtonVersionNumber;
+
+//! Project version string for BFSlidingButton.
+FOUNDATION_EXPORT const unsigned char BFSlidingButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BFSlidingButton/PublicHeader.h>
+
+
